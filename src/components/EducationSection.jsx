b65789931337c9e59dd1c6de1b482db5685d53e3
sskutushev <sskutushev@gmail.com>
2025-08-29 11:12:02 +0300

    style: fix responsive styles on landing page

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -15,16 +15,12 @@ const EducationSection = () => {
           <p>Независимо от того, школьник ты, студент или взрослый — здесь ты найдёшь знания, которые помогут тебе расти и зарабатывать</p>
           <h3>Основные преимущества для учеников:</h3>
           <div className="features-list">
-            <div className="features-column">
               <p><img src={import.meta.env.BASE_URL + 'mark.svg'} alt="" />Удобный личный кабинет</p>
               <p><img src={import.meta.env.BASE_URL + 'mark.svg'} alt="" />Возможность стать создателем курсов</p>
               <p><img src={import.meta.env.BASE_URL + 'mark.svg'} alt="" />Доступ к живому сообществу для участия в дискуссиях и поддержки</p>
-            </div>
-            <div className="features-column">
               <p><img src={import.meta.env.BASE_URL + 'mark.svg'} alt="" />Широкий выбор курсов, материалов и преподавателей</p>
               <p><img src={import.meta.env.BASE_URL + 'mark.svg'} alt="" />Геймификация и награды (Доступно скоро)</p>
               <p><img src={import.meta.env.BASE_URL + 'mark.svg'} alt="" />Реальные отзывы с живой системой рейтинга</p>
-            </div>
           </div>
           <Link to="/education" className="education-details-btn">Подробнее</Link>
         </div>
